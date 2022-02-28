8f0365f91bae1fffa4428284bd0c15be3368c0e3
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-02-28 02:20:55 +0100

    ExpenseDate custom compenent added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate.js";
 
 const ExpenceItem = function (props) {
   return (
     <div className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
