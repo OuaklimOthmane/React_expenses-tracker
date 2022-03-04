523218ed960b47835b44fecdb40e5fff4214e7b0
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-03-04 18:55:05 +0100

    Render lists of Data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 
 const Expenses = function (props) {
-  const [filteredyear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const changeFilteredYearHandler = (pickedYear) => {
     setFilteredYear(pickedYear);
@@ -15,11 +15,12 @@ const Expenses = function (props) {
   return (
     <Card className="expenses">
       <ExpensesFilter
-        selectedYear={filteredyear}
+        selectedYear={filteredYear}
         onChangeFilteredYear={changeFilteredYearHandler}
       />
       {props.items.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
@@ -30,3 +31,8 @@ const Expenses = function (props) {
 };
 
 export default Expenses;
+
+//! the "key" prop :
+//? Almost every React application displays an array list of some kind using the method map. And React tells us that for each element of that list that we return for rendering, we must provide a unique key prop.
+//? Keys help React identify which items have changed, are added, or are removed. Keys should be given to the elements inside the array to give the elements a stable identity.
+//? When the state of your component changes, the render function will return a new tree of React elements, different to the previous/current one. React needs to figure out what are the differences to efficiently update the UI with the most recent changes. This process of matching both element trees is called "reconciliation". ==> This is why the "key" prop is needed, to solve this issue. When children have keys, React uses the key to match children in the old tree with children in the new tree.
