03bba4beee29553ad8d82c9b8aac644d64beb19a
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-03-05 02:24:02 +0100

    Fix bug

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -19,7 +19,7 @@ const ExpenseForm = function (props) {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
